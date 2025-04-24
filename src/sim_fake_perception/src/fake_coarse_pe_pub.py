1b0d6a27a89750aea4a75d3927e3a752e1352263
Tom Gao <zimingg@andrew.cmu.edu>
2025-04-23 21:45:16 -0400

    Multiple peppers for coarse

--- src/sim_fake_perception/src/fake_coarse_pe_pub.py
+++ src/sim_fake_perception/src/fake_coarse_pe_pub.py
@@ -3,10 +3,12 @@
 import rospy
 from geometry_msgs.msg import Pose
 from shape_msgs.msg import SolidPrimitive
-from vader_msgs.msg import Pepper, Fruit, Peduncle
+from vader_msgs.msg import Pepper, Fruit, Peduncle, SimulationPepperSequence, SimulationPopPepper
 import random
 from tf.transformations import quaternion_from_euler
 import numpy as np
+
+pepper_sequence = None
 
 def create_pepper(fruit_pose, fruit_shape, peduncle_shape):
     pepper = Pepper()
@@ -33,10 +35,23 @@ def create_pepper(fruit_pose, fruit_shape, peduncle_shape):
 def get_gaussian_xyz_noise(xyz_noise):
     return np.random.normal(0, xyz_noise, 3)
 
+def _get_sim_pepper_sequence(data):
+    global pepper_sequence
+    pepper_sequence = data.pepper_poses
+    print("peppers:", pepper_sequence)
+
+def _pop_current_pepper(data):
+    global pepper_sequence
+    if len(pepper_sequence) > 0:
+        pepper_sequence.pop(0)
+        print("popped")
+
 def publisher():
     rospy.init_node('vader_pepper_publisher', anonymous=True)
-    pepper_pose = rospy.get_param('~sim_pepper_pose', "0.0 0.0 0.0 0.0 0.0 0.0")
-    pepper_pose = list(map(float, pepper_pose.split(" ")))
+    # pepper_pose = rospy.get_param('~sim_pepper_pose', "0.0 0.0 0.0 0.0 0.0 0.0")
+    # pepper_pose = list(map(float, pepper_pose.split(" ")))
+    pepper_seq_sub = rospy.Subscriber("/pepper_sequence", SimulationPepperSequence, _get_sim_pepper_sequence)
+    pepper_pop_sub = rospy.Subscriber("/pepper_pop", SimulationPopPepper, _pop_current_pepper)
     xyz_noise = rospy.get_param('~xyz_noise', "0.01")
     fruit_shape = rospy.get_param('~fruit_shape', "0.08 0.035")
     fruit_shape = list(map(float, fruit_shape.split(" ")))
@@ -53,18 +68,10 @@ def publisher():
     pub_hz = rospy.get_param('~pub_hz', "10")
     pub_topic = rospy.get_param('~pub_topic', "/fruit_coarse_pose")
 
-    gt_pose = Pose()
-    gt_pose.position.x = float(pepper_pose[0])
-    gt_pose.position.y = float(pepper_pose[1])
-    gt_pose.position.z = float(pepper_pose[2])
+    global pepper_sequence
+    while pepper_sequence is None:
+        rospy.sleep(0.1)
 
-    # Set the orientation of gt_pose
-    gt_pose.orientation.x = 0.
-    gt_pose.orientation.y = 0.
-    gt_pose.orientation.z = 0.
-    gt_pose.orientation.w = 1.
-
-    print("Coarse fruit pose estimate has ground truth =", gt_pose)
     pub = rospy.Publisher(pub_topic, Pepper, queue_size=10)
     rate = rospy.Rate(pub_hz) 
 
@@ -74,15 +81,21 @@ def publisher():
         # Add noise to the gt_pose
         _noise_xyz = get_gaussian_xyz_noise(xyz_noise)
         _pepper_pose = Pose()
-        _pepper_pose.position.x = gt_pose.position.x + _noise_xyz[0]
-        _pepper_pose.position.y = gt_pose.position.y + _noise_xyz[1]
-        _pepper_pose.position.z = gt_pose.position.z + _noise_xyz[2]
-        _pepper_pose.orientation = gt_pose.orientation
+        if pepper_sequence is None or len(pepper_sequence) == 0:
+            rospy.loginfo("No pepper sequence available")
+            break
+        _pepper_pose.position.x = pepper_sequence[0].position.x + _noise_xyz[0]
+        _pepper_pose.position.y = pepper_sequence[0].position.y + _noise_xyz[1]
+        _pepper_pose.position.z = pepper_sequence[0].position.z + _noise_xyz[2]
+        _pepper_pose.orientation.x = 0.
+        _pepper_pose.orientation.y = 0.
+        _pepper_pose.orientation.z = 0.
+        _pepper_pose.orientation.w = 1.
 
         # Create pepper
         pepper = create_pepper(_pepper_pose, fruit_shape, peduncle_shape)
 
-        # rospy.loginfo(f"Publishing Coarse Pose Pepper: {pepper}")
+        rospy.loginfo(f"Publishing Coarse Pose Pepper: {pepper}")
         pub.publish(pepper)
         rate.sleep()
 
